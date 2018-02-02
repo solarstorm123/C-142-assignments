de71be85711949146fbb6737dd432082b7b1a4c6
solarstorm123 <31160301+solarstorm123@users.noreply.github.com>
2018-02-02 23:59:18 +0530

    Update ques_1_a.cpp

--- sem2_lab_4/ques_1_a.cpp
+++ sem2_lab_4/ques_1_a.cpp
@@ -15,20 +15,20 @@ public:
     head=NULL;
     tail=NULL;}
 
-    void push(int x);
-    void pop();
-    void display();
+    void push(int x);       //function to push or add a new data in the stack.
+    void pop();             //function to delete the last element added.   
+    void display();         //function to display the current elements in the stack.
 };
 
-void StackLL::push(int x){
-node*temp=new node;
-if (head==NULL){
+void StackLL::push(int x){   
+node*temp=new node;            //creating a pointer temp which points to a new node.
+if (head==NULL){               //if the list is empty.
     head=temp;
     temp->data=x;
     temp->next=NULL;
     tail=temp;
 }
-else{
+else{                         //if the list already has atleast one element;
         temp->next=tail;
         tail=temp;
         tail->data=x;
@@ -37,19 +37,19 @@ cout<<x<<" is pushed."<<endl;
 };
 
 void StackLL::pop(){
-    if(tail!=NULL){
+    if(tail!=NULL){           //if the list has atleat one element.
     node*temp;
     temp=tail;
     cout<<tail->data<<" is popped."<<endl;
     delete tail;
     tail=temp->next;
     }
-    else{
+    else{                    //the list does not have any element.
         cout<<"stack underflow"<<endl;
     }
 }
 
-void StackLL::display(){
+void StackLL::display(){     //displays the list in the reverse order.
     node*temp=tail;
     cout<<"stack in reverse :";
     while(temp!=NULL){
